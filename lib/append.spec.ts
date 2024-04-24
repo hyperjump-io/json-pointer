516d221ae85dbc2f42b2f07e94b6f5a65d2af39b
Jason Desrosiers <jdesrosi@gmail.com>
2024-04-23 17:15:28 -0700

    Switch to vitest

--- lib/append.spec.ts
+++ lib/append.spec.ts
@@ -1,19 +1,19 @@
-import { expect } from "chai";
+import { describe, expect, test } from "vitest";
 import * as JsonPointer from "./index.js";
 
 
 describe("append", () => {
-  it("should append a segment to a pointer", () => {
+  test("should append a segment to a pointer", () => {
     const subject = JsonPointer.append("bar", "/foo");
     expect(subject).to.eql("/foo/bar");
   });
 
-  it("should append a segment to the nil pointer", () => {
+  test("should append a segment to the nil pointer", () => {
     const subject = JsonPointer.append("bar", JsonPointer.nil);
     expect(subject).to.eql("/bar");
   });
 
-  it("should escape a segment when it is appended to a pointer", () => {
+  test("should escape a segment when it is appended to a pointer", () => {
     const subject = JsonPointer.append("b~a/r", "/foo");
     expect(subject).to.eql("/foo/b~0a~1r");
   });
